fb36601d894d1518bb168b7a8a3df1a0bdf70195
DAB0mB <emanor6@gmail.com>
2018-08-22 04:00:14 +0800

    Fix bug where promise resolved is used accidently

--- src/utils/requireText.js
+++ src/utils/requireText.js
@@ -1,16 +1,16 @@
 import { readFile, readFileSync } from 'fs'
-import resolve from 'resolve'
+import resolvePath from 'resolve'
 
 const cache = {}
 
 const requireText = (path) => {
-  path = resolve.sync(path)
+  path = resolvePath.sync(path)
 
   return cache[path] = cache[path] || readFileSync(path).toString()
 }
 
 requireText.promise = (path) => new Promise((resolve, reject) => {
-  resolve(path, (err, path) => {
+  resolvePath(path, (err, path) => {
     if (err) {
       return reject(err)
     }
